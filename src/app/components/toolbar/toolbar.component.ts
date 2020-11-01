9cf4654f5526d00375ac2fe9c28d7dd9679ed02c
Rolands Jegorovs <rolandjegorov@gmail.com>
2020-11-01 23:21:42 +0200

    Add toolbar actions, add Caveat Font family, add My Images header

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,6 +1,11 @@
-import { Component, ViewChild, ElementRef, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { ImageUploadService } from '../image-upload/image-upload.service';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+
+interface Action {
+  type: string;
+  callback: () => void;
+}
 
 @Component({
   selector: 'app-toolbar',
@@ -9,11 +14,34 @@ import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ToolbarComponent {
-  constructor(public imageService: ImageUploadService, private sanitizer: DomSanitizer) {}
-
-  @ViewChild('imgTarget') imgTarget!: ElementRef<HTMLSpanElement>;
+  public currentItem: Action | HTMLImageElement | undefined;
+  public actions: Action[] = [
+    {
+      type: 'delete',
+      callback: () => { console.log('delete'); },
+    },
+  ];
+  constructor(
+    public imageService: ImageUploadService,
+    private sanitizer: DomSanitizer,
+  ) {}
 
   public sanitize(url: string): SafeUrl {
     return this.sanitizer.bypassSecurityTrustUrl(url);
+  }
+
+  public onToolbarItemClick(item: Action | HTMLImageElement): void {
+    if (this.isAction(item)) {
+      this.imageService.setCurrentImage(null);
+      item.callback();
+    } else {
+      this.imageService.setCurrentImage(item);
+    }
+
+    this.currentItem = item;
+  }
+
+  private isAction(item: Action | HTMLImageElement): item is Action {
+    return !!(item as Action).callback;
   }
 }
