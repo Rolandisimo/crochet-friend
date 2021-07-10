6aa5e359373a19c9ae2c484c2de2cb6e7faf83ea
Rolands Jegorovs <rolandjegorov@gmail.com>
2021-07-10 19:59:39 +0300

    Migrate to angular 11

--- src/app/components/symbol-grid/symbol-grid.component.spec.ts
+++ src/app/components/symbol-grid/symbol-grid.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SymbolGridComponent } from './symbol-grid.component';
 
@@ -6,7 +6,7 @@ describe('SymbolGridComponent', () => {
   let component: SymbolGridComponent;
   let fixture: ComponentFixture<SymbolGridComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SymbolGridComponent ]
     })
